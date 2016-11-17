4ac5d1cb884fe0004b5a065b16ec84c23f3e5341
Christian Niemann <spirem@gmail.com>
2016-11-17 11:48:21 +0100

    wrote testStub for gameController

---
+++ src/test/gameControllerTestStub.java
@@ -0,0 +1,43 @@
+package test;
+
+import entity.GameBoard;
+import entity.Player;
+import entity.fields.Field;
+
+public class gameControllerTestStub {
+	
+	/**
+	 * Method that implements the exact same logic as our gameController's playTurn, just without all the GUI interaction.
+	 * Remember to set the player onField and taxChoice attributes before you inject the player, and remember that the taxChoice will be reset
+	 * to false whenever the player is actually taxed.
+	 * @param gameBoard - use the default gameBoard constructor to create this object.
+	 * @param player - the player who is landing on the field.
+	 * @param wantToBuy - position on whether to buy an unowned field or not, true = wants to buy, false = doesn't want to buy.
+	 */
+	public void playTurnTest(GameBoard gameBoard, Player player, boolean wantToBuy) {
+		int fieldNumber = player.getOnField();
+		Field field = gameBoard.getField(fieldNumber);
+		if(field.isOwnable())
+		{
+			Player ownerOfField = field.getOwner();
+			if(ownerOfField == null)
+			{
+				int priceOfField = field.getPrice();
+				if(wantToBuy)
+				{
+					if(player.getBankAccount().getBalance() > priceOfField)
+					{
+						player.getBankAccount().withdraw(priceOfField);
+						field.buyField(player);
+					}
+				}
+			} else
+			{
+				field.landOnField(player);
+			}
+		} else
+		{
+			field.landOnField(player);
+		}
+	}
+}
