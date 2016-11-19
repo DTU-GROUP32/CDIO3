7ad6738160d648eb28de9543e1d904e5a0e55a80
Unknown <vpmads@gmail.com>
2016-11-19 12:05:31 +0100

    Test

--- src/test/GameLogikTest.java
+++ src/test/GameLogikTest.java
@@ -3,32 +3,43 @@ package test;
 import static org.junit.Assert.*;
 
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-
+import entity.*;
+import entity.fields.*;
 public class GameLogikTest {
-
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
+	
+private Player player; 
+private int balance;
+private GameBoard gameBoard;
+private int fieldNumber;
+Field field;
 	@Before
 	public void setUp() throws Exception {
+		balance = 1000;
+		player = new Player("Test",balance);
+		field = gameBoard.getField(fieldNumber);
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		balance = 1000;
+		player = null; 
+		field = null;
+		
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+	public void testLandOnField500() {
+      	int expected = 1000;
+      	int actual = player.getBankAccount().getBalance();
+   assertEquals(expected, actual);
+   fieldNumber = 6;
+   field.landOnField(player);
+   int expected1 = 1500;
+   int actual1 = player.getBankAccount().getBalance();
+      assertEquals(expected1,actual1);
 
+
+	}
 }
