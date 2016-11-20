c0890881943a034bc8e8f1c79380d6c407025691
Unknown <vpmads@gmail.com>
2016-11-20 19:57:45 +0100

    Test (almost complete)

--- src/test/GameLogikTest.java
+++ src/test/GameLogikTest.java
@@ -10,30 +10,19 @@ import entity.fields.*;
 public class GameLogikTest {
 
 	private Player player; 
-	private Player player2; 
-	private Player owner;
-	private int balance;
-	private int rent; 
-	private int price; 
-	private int lastroll;
-	private int bonus;
+	private Player player2;
 	private Field territory;
 	private Field refuge;
 	private Field laborcamp;
 	private Field tax; 
 	private Field fleet; 
+	
 	@Before
 	public void setUp() throws Exception {
-		balance = 1000;
-		bonus = 500;
-		rent = 100; 
-		price = 1000; 
-		lastroll = 6; 
-		owner = player2; 
-		player = new Player("Test",balance);
-		player2 = new Player("TestOwner",balance);
-		refuge = new Refuge(bonus);
-		territory = new Territory(price,rent);
+		player = new Player("Test",5000);
+		player2 = new Player("TestOwner",5000);
+		refuge = new Refuge(500);
+		territory = new Territory(1000,100);
 		laborcamp = new LaborCamp();
 		tax = new Tax(2000);
 		fleet = new Fleet();
@@ -42,13 +31,13 @@ public class GameLogikTest {
 	@After
 	public void tearDown() throws Exception {
 		player = null; 
-		player = null;
 		player2 = null;
 		refuge = null; 
-		owner = null;  
 		territory = null; 
 		laborcamp = null; 
-
+		tax = null;
+		fleet = null; 
+		
 	}
 
 	/**
@@ -56,11 +45,11 @@ public class GameLogikTest {
 	 */
 	@Test
 	public void testLandOnFieldRefuge() {
-		int expected = 1000;
+		int expected = 5000;
 		int actual = player.getBankAccount().getBalance();
 		assertEquals(expected, actual);
 		refuge.landOnField(player);
-		expected = 1000+500;
+		expected = 5000+500;
 		actual = player.getBankAccount().getBalance();
 		assertEquals(expected, actual);
 	}
@@ -71,24 +60,28 @@ public class GameLogikTest {
 	
 	@Test
 	public void testLandOnFieldTerritory() {
-		int expected = 1000;
+		int expected = 5000;
 		int actual = player.getBankAccount().getBalance();
 		assertEquals(expected, actual);
+		territory.setOwner(player2);
 		territory.landOnField(player);
-		expected = 1000-100;
+		expected = 4900;
 		actual = player.getBankAccount().getBalance();
 		assertEquals(expected, actual);
 	}
-/**
- * Tests the field type LaborCamp. The expected result is player bank account loses 600 because last roll was 6. 
- */
+	
+	/**
+	 * Tests the field type LaborCamp. The expected result is player bank account loses 600 because last roll was 6. 
+	 */
 	@Test
 	public void testLandOnFieldLaborCamp() {
-		int expected = 1000;
+		int expected = 5000;
 		int actual = player.getBankAccount().getBalance();
 		assertEquals(expected, actual);
+		player.setLastRoll(6);
+		laborcamp.setOwner(player2);
 		laborcamp.landOnField(player);
-		expected = 1000-600;
+		expected = 5000-600;
 		actual = player.getBankAccount().getBalance();
 		assertEquals(expected, actual);
 	}
@@ -97,11 +90,11 @@ public class GameLogikTest {
 	 */
 	@Test
 	public void testLandOnFieldTax() {
-		int expected = 3000;
-		int actual = player.getBankAccount().getBalance()+2000;
+		int expected = 5000;
+		int actual = player.getBankAccount().getBalance();
 		assertEquals(expected, actual);
 		tax.landOnField(player);
-		expected = 1000;
+		expected = 3000;
 		actual = player.getBankAccount().getBalance();
 		assertEquals(expected, actual);
 	}
@@ -110,11 +103,11 @@ public class GameLogikTest {
 	 */
 	@Test
 	public void testLandOnFieldFleet() {
-		int expected = 3000;
-		int actual = player.getBankAccount().getBalance()+2000;
+		int expected = 5000;
+		int actual = player.getBankAccount().getBalance();
 		assertEquals(expected, actual);
 		fleet.landOnField(player);
-		expected = 1000;
+		expected = 3000;
 		actual = player.getBankAccount().getBalance();
 		assertEquals(expected, actual);
 	}
