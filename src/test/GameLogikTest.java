c94330ca73524a6989a93af60fa49e3e23576553
Unknown <vpmads@gmail.com>
2016-11-20 17:58:28 +0100

    Test udkast

--- src/test/GameLogikTest.java
+++ src/test/GameLogikTest.java
@@ -8,38 +8,72 @@ import org.junit.Test;
 import entity.*;
 import entity.fields.*;
 public class GameLogikTest {
-	
-private Player player; 
-private int balance;
-private GameBoard gameBoard;
-private int fieldNumber;
-Field field;
+
+	private Player player; 
+	private Player player2; 
+	private int balance;
+	private Field refuge;
+	private int bonus;
+	private Field territory;
+	protected Player owner;
+	private int rent; 
+	private int price; 
+
 	@Before
 	public void setUp() throws Exception {
 		balance = 1000;
+		bonus = 500;
 		player = new Player("Test",balance);
-		field = gameBoard.getField(fieldNumber);
+		player2 = new Player("TestOwner",balance);
+		refuge = new Refuge(bonus);
+		owner = player2; 
+		rent = 100; 
+		price = 1000; 
+		territory = new Territory(price,rent);
 	}
 
 	@After
 	public void tearDown() throws Exception {
-		balance = 1000;
 		player = null; 
-		field = null;
-		
+		player = null;
+		player2 = null;
+		refuge = null; 
+		owner = null;  
+		territory = null; 
+
 	}
 
 	@Test
-	public void testLandOnField500() {
-      	int expected = 1000;
-      	int actual = player.getBankAccount().getBalance();
-   assertEquals(expected, actual);
-   fieldNumber = 6;
-   field.landOnField(player);
-   int expected1 = 1500;
-   int actual1 = player.getBankAccount().getBalance();
-      assertEquals(expected1,actual1);
+	public void testLandOnFieldRefuge500() {
+		int expected = 1000;
+		int actual = player.getBankAccount().getBalance();
+		assertEquals(expected, actual);
+		refuge.landOnField(player);
+		expected = 1000+500;
+		actual = player.getBankAccount().getBalance();
+		assertEquals(expected, actual);
+	}
 
+	@Test
+	public void testLandOnFieldRefugeTwice500() {
+		int expected = 1000;
+		int actual = player.getBankAccount().getBalance();
+		assertEquals(expected, actual);
+		refuge.landOnField(player);
+		refuge.landOnField(player);
+		expected = 1000+500+500;
+		actual = player.getBankAccount().getBalance();
+		assertEquals(expected, actual);
 
+	}
+	@Test
+	public void testLandOnFieldTerritory500() {
+		int expected = 1000;
+		int actual = player.getBankAccount().getBalance();
+		assertEquals(expected, actual);
+		territory.landOnField(player);
+		expected = 1000-100;
+		actual = player.getBankAccount().getBalance();
+		assertEquals(expected, actual);
 	}
 }
